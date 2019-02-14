753492a3e7642fef298c93ecc0d326c476e8def3
Daniel Flood <daniel.flood-1@uts.edu.au>
2019-02-15 10:30:49 +1100

    splitting off py resolver code to separate repo, moving out of turret-server

---
+++ package.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+name = 'turret_resolver'
+
+version = '0.0.1'
+
+authors = ['wen.tan',
+           'ben.skinner',
+           'daniel.flood']
+
+build_requires = ['python']
+
+def commands():
+    env.PYTHONPATH.append('{root}/python')
